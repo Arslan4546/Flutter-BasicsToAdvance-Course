89f8f06243ce695d288a1ed524b3d7f23c93e033
Arslan Tariq <arslantariq4546@gmail.com>
2024-11-11 23:05:08 +0500

    add some responsiveness practices

---
+++ MediaQueury/Project/media_queury_practic/lib/ResponsiveDesign1.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+ void main(){
+   runApp(const Boxes());
+ }
+class Boxes extends StatelessWidget {
+  const Boxes({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.white,
+        body: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              Flexible(
+                flex: 6,
+                child: Row(
+                  children: [
+                    Expanded(
+                        flex: 9,
+                        child:Container(
+                          decoration: BoxDecoration(
+                              color: Colors.red,
+                              borderRadius: BorderRadius.circular(20)
+                          ),
+                        )),
+                    const SizedBox(width: 10,),
+                    Expanded(
+                      flex:9,
+                      child:Container(
+                          decoration: BoxDecoration(
+                              color: Colors.red,
+                              borderRadius: BorderRadius.circular(20)
+                          )),
+                    )
+                  ],
+                ),
+              ),
+              const SizedBox(height: 10,),
+              Flexible(
+                  flex: 6,
+                  child: Row(
+                    children: [
+                      Expanded(
+                          child: Container(
+                              decoration: BoxDecoration(
+                                  color: Colors.green,
+                                  borderRadius: BorderRadius.circular(20)
+                              )
+                          )
+                      )
+                    ],
+                  )),
+              const SizedBox(height: 10,),
+              Flexible(
+                flex: 10,
+                child: Row(
+                  children: [
+                    Expanded(
+                        flex: 9,
+                        child:Container(
+                          decoration: BoxDecoration(
+                              color: Colors.cyan,
+                              borderRadius: BorderRadius.circular(20)
+                          ),
+                        )),
+                    const  SizedBox(width: 10,),
+                    Expanded(
+                      flex:9,
+                      child:Container(
+                          decoration: BoxDecoration(
+                              color: Colors.cyan,
+                              borderRadius: BorderRadius.circular(20)
+                          )),
+                    )
+                  ],
+      
+                ),
+              ),
+      
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
