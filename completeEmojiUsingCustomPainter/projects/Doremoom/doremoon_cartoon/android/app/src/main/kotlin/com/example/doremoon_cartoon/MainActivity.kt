195bb242ce2a91635b3252892cee08edb16773b3
Arslan Tariq <arslantariq4546@gmail.com>
2025-02-11 09:11:46 +0500

    add some practices

---
+++ completeEmojiUsingCustomPainter/projects/Doremoom/doremoon_cartoon/android/app/src/main/kotlin/com/example/doremoon_cartoon/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.doremoon_cartoon
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ completeEmojiUsingCustomPainter/projects/Doremoom/doremoon_cartoon/lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:doremoon_cartoon/doremoon.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: Home());
+  }
+}
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(child: Doremoon(size: Size(200, 200))),
+    );
+  }
+}
