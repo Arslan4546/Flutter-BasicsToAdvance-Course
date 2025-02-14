0e5e40a389d3e0f2467eeb0ea2287c9a62e2c399
Arslan Tariq <arslantariq4546@gmail.com>
2025-02-14 09:15:35 +0500

    add images of animations practices

--- tweenInAnimation/project/tween_animation_builder/lib/main.dart
+++ tweenInAnimation/project/tween_animation_builder/lib/main.dart
@@ -31,59 +31,104 @@ class _TweenAnimationState extends State<TweenAnimation> {
   bool _isExpanded = false;
   Color _startColor = Colors.blue;
   Color _endColor = Colors.purple;
+  final List<Color> _colors = [
+    Colors.blue,
+    Colors.purple,
+    Colors.orange,
+    Colors.green,
+  ];
+  int _colorIndex = 0;
 
   void _toggleAnimation() {
     setState(() {
       _isExpanded = !_isExpanded;
-      // Swap colors for next animation
-      final tempColor = _startColor;
-      _startColor = _endColor;
-      _endColor = tempColor;
+      // Cycle through colors
+      _colorIndex = (_colorIndex + 1) % _colors.length;
+      _startColor = _colors[_colorIndex];
+      _endColor = _colors[(_colorIndex + 1) % _colors.length];
     });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey[200],
+      backgroundColor: Colors.black,
       appBar: AppBar(
-        title: const Text('Beautiful Tween Animation'),
+        title: const Text('Magical Tween Animation'),
+        backgroundColor: Colors.transparent,
+        elevation: 0,
       ),
       body: Center(
         child: GestureDetector(
           onTap: _toggleAnimation,
           child: TweenAnimationBuilder(
-            duration: const Duration(milliseconds: 800),
+            duration: const Duration(milliseconds: 1200),
             tween: Tween<double>(
               begin: 0,
               end: _isExpanded ? 1.0 : 0.0,
             ),
-            curve: Curves.easeInOutBack,
+            curve: Curves.elasticOut,
             builder: (context, double value, child) {
-              return Transform.scale(
-                scale: 0.8 + (value * 0.2),
+              return Transform(
+                alignment: Alignment.center,
+                transform: Matrix4.identity()
+                  ..setEntry(3, 2, 0.001)
+                  ..rotateY(value * 2 * 3.14159)
+                  ..rotateX(value * 3.14159 / 4),
                 child: Container(
-                  width: 200 + (value * 100),
-                  height: 200 + (value * 100),
+                  width: 200 + (value * 150),
+                  height: 200 + (value * 150),
                   decoration: BoxDecoration(
-                    color: Color.lerp(_startColor, _endColor, value),
-                    borderRadius: BorderRadius.circular(20 + (value * 20)),
+                    gradient: LinearGradient(
+                      begin: Alignment.topLeft,
+                      end: Alignment.bottomRight,
+                      colors: [
+                        Color.lerp(_startColor, _endColor, value)!,
+                        Color.lerp(_endColor, _startColor, value)!,
+                      ],
+                    ),
+                    borderRadius: BorderRadius.circular(value * 40),
                     boxShadow: [
                       BoxShadow(
-                        color: Colors.black.withOpacity(0.2),
-                        blurRadius: 20,
-                        offset: Offset(0, 10 * value),
+                        color: _startColor.withOpacity(0.5),
+                        blurRadius: 30 * value,
+                        spreadRadius: 10 * value,
+                      ),
+                      BoxShadow(
+                        color: _endColor.withOpacity(0.5),
+                        blurRadius: 30 * value,
+                        spreadRadius: 10 * value,
                       ),
                     ],
                   ),
                   child: Center(
-                    child: Text(
-                      'Tap Me!',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 24 + (value * 8),
-                        fontWeight: FontWeight.bold,
-                      ),
+                    child: Stack(
+                      alignment: Alignment.center,
+                      children: [
+                        Transform.rotate(
+                          angle: value * 4 * 3.14159,
+                          child: Icon(
+                            Icons.star,
+                            size: 80 + (value * 40),
+                            color: Colors.white.withOpacity(0.8),
+                          ),
+                        ),
+                        Text(
+                          'Magic!',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 32 + (value * 16),
+                            fontWeight: FontWeight.bold,
+                            shadows: [
+                              Shadow(
+                                color: Colors.black26,
+                                blurRadius: 10,
+                                offset: Offset(5 * value, 5 * value),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
                     ),
                   ),
                 ),
