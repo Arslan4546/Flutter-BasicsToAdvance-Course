ea8e7abd100390074b98ea96f4582f2be49e5bfb
Arslan Tariq <arslantariq4546@gmail.com>
2025-02-25 10:31:39 +0500

    add some projects

---
+++ AnimatedBuilder/projects/animated_builder/lib/main.dart
@@ -0,0 +1,95 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart'
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _animation;
+  late Animation<double> _scaleAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller =
+        AnimationController(vsync: this, duration: const Duration(seconds: 1));
+    _animation = Tween<double>(begin: 0, end: 2 * pi).animate(_controller);
+    _scaleAnimation = Tween<double>(begin: 1, end: 0.5).animate(_controller);
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var src =
+        'https://images.unsplash.com/photo-1568901346375-23c9450c58cd?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MXxZWFyY2h8Mnx8YnV2ZyVvYfGVufDB8fDB8Hw==';
+
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            AnimatedBuilder(
+              animation: _animation,
+              builder: (context, child) => Transform.rotate(
+                angle: _animation.value,
+                child: Transform.scale(
+                  scale: _scaleAnimation.value,
+                  child: Opacity(
+                    opacity: _scaleAnimation.value,
+                    child: child,
+                  ),
+                ),
+              ),
+              child: Image.network(src),
+            ),
+          
+            ElevatedButton(
+              onPressed: () {
+                _controller.forward();
+              },
+              child: const Text('Play'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
