622e9dff60532a2a019c69325309368a304a7a40
Arslan Tariq <arslantariq4546@gmail.com>
2025-04-25 15:52:25 +0500

    add navigation guide

--- Navigator1.0 Start/NavigationWhichWeActullyUse/project/actual_usage_navigation1/lib/main.dart
+++ Navigator1.0 Start/NavigationWhichWeActullyUse/project/actual_usage_navigation1/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:actual_usage_navigation1/navigation_Page.dart';
+import 'package:actual_usage_navigation1/second_page.dart';
+import 'package:actual_usage_navigation1/third_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -8,33 +10,47 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      // routes:
+      // {
+      //   MyHomePage.pageName: (context) => MyHomePage(title: 'Home Page'),
+      //   SecondPage.pageName: (context) => SecondPage(data: 'Pakistan Zindabad'),
+      //   ThirdPage.pageName: (context) => ThirdPage(),
+      // },
       onGenerateRoute: onGenerateRoute,
       initialRoute: MyHomePage.pageName,
+      //onGenerateRoute: MyNavigation.onGenerateRoute,
+      // home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-  static const String pageName = '/';
 
   final String title;
+  static const String pageName = '/';
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  String data = 'You have pushed the button this many times:';
+
+  void _incrementCounter() {
+    Navigator.of(context).pushNamed(
+      SecondPage.pageName,
+      arguments: 'Pakistan Zindabad',
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -46,12 +62,44 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
+            Text(data),
           ],
         ),
       ),
+      floatingActionButton: OverflowBar(
+        children: [
+          FloatingActionButton(
+            onPressed: _incrementCounter,
+            heroTag: 'inc',
+            tooltip: 'Increment',
+            child: const Icon(Icons.add),
+          ),
+          FloatingActionButton(
+            onPressed: _decrementCounter,
+            heroTag: 'dec',
+            tooltip: 'Decrement',
+            child: const Icon(Icons.bubble_chart),
+          ),
+        ],
+      ),
     );
+  }
+
+  void _decrementCounter() async {
+    // var result = await Navigator.of(context).push(
+    //   MaterialPageRoute(
+    //     builder: (context) => ThirdPage(),
+    //     settings: RouteSettings(arguments: 'Pakistan Zindabad'),
+    //   ),
+    // );
+
+    var result = await Navigator.of(
+      context,
+    ).pushNamed(ThirdPage.pageName, arguments: 'CAS is the Best') as String;
+
+    ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text(result)));
+    setState(() {
+      data = result;
+    });
   }
 }
