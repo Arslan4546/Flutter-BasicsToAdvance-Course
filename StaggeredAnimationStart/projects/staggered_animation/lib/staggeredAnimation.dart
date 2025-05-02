84ba0271993a55c339e93917038a68df0de4fa2c
Arslan Tariq <arslantariq4546@gmail.com>
2025-05-02 16:46:20 +0500

    staggered aniamation porject updated

--- StaggeredAnimationStart/projects/staggered_animation/lib/staggeredAnimation.dart
+++ StaggeredAnimationStart/projects/staggered_animation/lib/staggeredAnimation.dart
@@ -1,6 +1,17 @@
 import 'dart:math';
 import 'package:flutter/material.dart';
 
+void main() {
+  runApp(const MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: Scaffold(
+      body: Center(
+        child: AnimatedMenu(),
+      ),
+    ),
+  ));
+}
+
 class AnimatedMenu extends StatefulWidget {
   const AnimatedMenu({super.key});
 
