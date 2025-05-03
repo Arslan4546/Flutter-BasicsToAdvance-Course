1185df10f955a358bd9380b112c3d537412ea138
Arslan Tariq <arslantariq4546@gmail.com>
2025-05-03 10:26:06 +0500

    update

---
+++ Navigation2.0/project/navigation2_practice/lib/myRouterDelegate.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:navigation2_practice/main.dart';
+import 'package:navigation2_practice/myPageBuilder.dart';
+
+class MyRouterDelegate extends RouterDelegate<RouteSettings>
+    with ChangeNotifier, PopNavigatorRouterDelegateMixin {
+  static MyRouterDelegate of(BuildContext context) {
+    final delegate = Router.of(context).routerDelegate;
+    assert(delegate is MyRouterDelegate);
+    return delegate as MyRouterDelegate;
+  }
+
+  final List<RouteSettings> _stack = [
+    const RouteSettings(name: MainPage.routeName)
+  ];
+
+  push(RouteSettings settings) {
+    _stack.add(settings);
+    notifyListeners();
+  }
+
+  pop() {
+    if (_stack.length > 1) {
+      _stack.removeLast();
+      notifyListeners();
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Navigator(
+      key: navigatorKey,
+      pages: [
+        for (RouteSettings settings in _stack) Mypagebuilder.buildPage(settings)
+      ],
+      // ignore: deprecated_member_use
+      onPopPage: onPopPage,
+    );
+  }
+
+  @override
+  RouteSettings? get currentConfiguration => _stack.isNotEmpty
+      ? _stack.last
+      : const RouteSettings(name: MainPage.routeName);
+
+  @override
+  Future<void> setInitialRoutePath(RouteSettings configuration) {
+    return setNewRoutePath(configuration);
+  }
+
+  @override
+  GlobalKey<NavigatorState>? get navigatorKey => GlobalKey<NavigatorState>();
+
+  @override
+  Future<void> setNewRoutePath(RouteSettings configuration) {
+    _stack
+      ..clear()
+      ..add(configuration);
+    notifyListeners();
+    return SynchronousFuture(null);
+  }
+
+  bool onPopPage(Route route, result) {
+    return !route.didPop(result);
+  }
+}
