e6ec4b51adb3a12245bdba829402fefb3e99bd6e
Arslan Tariq <arslantariq4546@gmail.com>
2025-05-03 09:35:34 +0500

    add pageBuilder

---
+++ Navigation2.0/project/navigation2_practice/lib/myPageBuilder.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:navigation2_practice/error_page.dart';
+import 'package:navigation2_practice/first_page.dart';
+import 'package:navigation2_practice/main.dart';
+import 'package:navigation2_practice/second_page.dart';
+
+class Mypagebuilder {
+  // this is the function for checking is the page is valid or not
+  static bool isValidPage(RouteSettings settings) {
+    return settings.name == '/main' ||
+        settings.name == '/first' ||
+        settings.name == '/second';
+  }
+
+// this is the fucntion for routing the main to exact location
+  static Page buildPage(RouteSettings settings) {
+    switch (settings.name) {
+      case '/main':
+        return MaterialPage(
+          child: const MainPage(),
+          arguments: settings.arguments,
+          name: settings.name,
+        );
+      case '/first':
+        return MaterialPage(
+          child: const FirstPage(),
+          arguments: settings.arguments,
+          name: settings.name,
+        );
+      case '/second':
+        return MaterialPage(
+          child: const SecondPage(),
+          arguments: settings.arguments,
+          name: settings.name,
+        );
+      default:
+        return MaterialPage(
+          child: const ErrorPage(),
+          arguments: settings.arguments,
+          name: settings.name,
+        );
+    }
+  }
+}
