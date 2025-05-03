b649b0507aab57dacbc7e8e4338cf19735796ae3
Arslan Tariq <arslantariq4546@gmail.com>
2025-05-03 09:55:35 +0500

    update

---
+++ Navigation2.0/project/navigation2_practice/lib/myRouteInformationParser.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:navigation2_practice/error_page.dart';
+import 'package:navigation2_practice/myPageBuilder.dart';
+
+class MyrouteInformationParser extends RouteInformationParser<RouteSettings> {
+  @override
+  Future<RouteSettings> parseRouteInformation(
+      RouteInformation routeInformation) {
+    // Parse the URI from the route information
+    var settings = RouteSettings(name: routeInformation.uri.toString());
+    if (!Mypagebuilder.isValidPage(settings)) {
+      settings = const RouteSettings(name: ErrorPage.routeName);
+    }
+    return SynchronousFuture(settings);
+  }
+
+  @override
+  RouteInformation? restoreRouteInformation(RouteSettings configuration) {
+    return RouteInformation(uri: Uri.parse(configuration.name!));
+  }
+}
