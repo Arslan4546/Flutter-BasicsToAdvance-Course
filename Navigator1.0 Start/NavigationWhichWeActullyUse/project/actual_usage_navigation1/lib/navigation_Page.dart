4e0b96e97ddc950081e6f32978e6f8b3fb247a75
Arslan Tariq <arslantariq4546@gmail.com>
2025-05-05 09:45:13 +0500

    update

--- Navigator1.0 Start/NavigationWhichWeActullyUse/project/actual_usage_navigation1/lib/navigation_Page.dart
+++ Navigator1.0 Start/NavigationWhichWeActullyUse/project/actual_usage_navigation1/lib/navigation_Page.dart
@@ -6,18 +6,26 @@ import 'package:actual_usage_navigation1/third_page.dart';
 import 'package:flutter/material.dart';
 
 Route<dynamic> onGenerateRoute(RouteSettings settings) {
-  if (settings.name == MyHomePage.pageName) {
-    return MaterialPageRoute(
-      builder: (context) => const MyHomePage(title: 'Home Page'),
-      settings: settings,
-    );
-  } else if (settings.name == SecondPage.pageName) {
-    return LeftSlidePageTransition(
-      page: SecondPage(data: settings.arguments as String),
-      settings: settings,
-    );
-  } else if (settings.name == ThirdPage.pageName) {
-    return LeftSlidePageTransition(page: const ThirdPage(), settings: settings);
+  switch (settings.name) {
+    case MyHomePage.pageName:
+      return MaterialPageRoute(
+        builder: (context) => const MyHomePage(title: 'Home Page'),
+        settings: settings,
+      );
+    case SecondPage.pageName:
+      return LeftSlidePageTransition(
+        page: SecondPage(data: settings.arguments as String),
+        settings: settings,
+      );
+    case ThirdPage.pageName:
+      return LeftSlidePageTransition(
+        page: const ThirdPage(),
+        settings: settings,
+      );
+    default:
+      return MaterialPageRoute(
+        builder: (context) => const ErrorPage(),
+        settings: settings,
+      );
   }
-  return MaterialPageRoute(builder: (context) => const ErrorPage());
 }
