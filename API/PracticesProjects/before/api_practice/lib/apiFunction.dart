af2a879f738847c451a1ccc5a06abd0bc6f3b433
Arslan Tariq <arslantariq4546@gmail.com>
2025-05-06 16:21:47 +0500

    update

---
+++ API/PracticesProjects/before/api_practice/lib/apiFunction.dart
@@ -0,0 +1,15 @@
+import 'dart:convert';
+import 'package:api_practice/ModelClass.dart';
+import 'package:http/http.dart' as http;
+
+Future<List<Post>> fetchPosts() async {
+  final response =
+      await http.get(Uri.parse('https://jsonplaceholder.typicode.com/posts'));
+
+  if (response.statusCode == 200) {
+    final List<dynamic> jsonList = json.decode(response.body);
+    return jsonList.map((json) => Post.fromJson(json)).toList();
+  } else {
+    throw Exception('Failed to load posts');
+  }
+}
