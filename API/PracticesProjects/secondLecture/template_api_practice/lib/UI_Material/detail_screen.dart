42158f17a26b63d05b254446ca63a4bb974d6f4a
Arslan Tariq <arslantariq4546@gmail.com>
2025-05-16 17:15:40 +0500

    implemented api practice

--- API/PracticesProjects/secondLecture/template_api_practice/lib/UI_Material/detail_screen.dart
+++ API/PracticesProjects/secondLecture/template_api_practice/lib/UI_Material/detail_screen.dart
@@ -6,15 +6,56 @@ class DetailScreen extends StatelessWidget {
   final ProductModel products;
   @override
   Widget build(context) {
-    return Container(
-      height: 300,
-      width: double.infinity,
-      child: Column(
-        children: [
-          Text(products.name),
-          Text(products.title),
-          Text(products.id.toString()),
-        ],
+    return Center(
+      child: Container(
+        height: 300,
+        width: double.infinity,
+        margin: EdgeInsets.all(16),
+        padding: EdgeInsets.all(20),
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [Colors.blueAccent, Colors.lightBlueAccent],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+          borderRadius: BorderRadius.circular(20),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black12,
+              blurRadius: 10,
+              spreadRadius: 2,
+              offset: Offset(5, 5),
+            ),
+          ],
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              products.name,
+              style: TextStyle(
+                fontSize: 22,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
+            ),
+            SizedBox(height: 10),
+            Text(
+              products.title,
+              style: TextStyle(
+                fontSize: 18,
+                fontStyle: FontStyle.italic,
+                color: Colors.white70,
+              ),
+            ),
+            SizedBox(height: 10),
+            Text(
+              'ID: ${products.id}',
+              style: TextStyle(fontSize: 16, color: Colors.white),
+            ),
+          ],
+        ),
       ),
     );
   }
