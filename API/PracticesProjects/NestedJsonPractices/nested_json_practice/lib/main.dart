04cfb3e4e9b97fee4db4312808bb17612e923d94
Arslan Tariq <arslantariq4546@gmail.com>
2025-05-17 08:42:26 +0500

    update

--- API/PracticesProjects/NestedJsonPractices/nested_json_practice/lib/main.dart
+++ API/PracticesProjects/NestedJsonPractices/nested_json_practice/lib/main.dart
@@ -15,15 +15,13 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
+  const MyHomePage({super.key});
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
@@ -45,7 +43,7 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
 
-        title: Text(widget.title),
+        title: const Text('Nested JSON Practice'),
       ),
       body: Center(
         child: FutureBuilder(
