d7bc0ac43e9ca80c65caced9ef887e585b2b3c29
Arslan Tariq <arslantariq4546@gmail.com>
2025-05-18 17:07:38 +0500

    update

---
+++ SQFLiteDataBase/lecture1/projects/sqflite_database_prractice1/lib/Layout/home.dart
@@ -0,0 +1,121 @@
+import 'package:flutter/material.dart';
+import 'package:sqflite_database_prractice1/DataBase/db_crud_service.dart';
+import 'package:sqflite_database_prractice1/Models/student_model.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final _formKey = GlobalKey<FormState>();
+  final _nameController = TextEditingController();
+  final _rollNoController = TextEditingController();
+  final _emailController = TextEditingController();
+  final _cgpaController = TextEditingController();
+  final _phoneController = TextEditingController();
+
+  final DbCrud _dbCrud = DbCrud();
+
+  Future<void> _saveStudent() async {
+    if (_formKey.currentState!.validate()) {
+      StudentModel student = StudentModel(
+        name: _nameController.text.trim(),
+        rollNo: int.parse(_rollNoController.text.trim()),
+        email: _emailController.text.trim(),
+        cGPA: double.parse(_cgpaController.text.trim()),
+        phone: _phoneController.text.trim(),
+      );
+
+      bool inserted = await _dbCrud.insertStudent(student);
+
+      if (inserted) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(content: Text('Student added successfully!')),
+        );
+        _nameController.clear();
+        _rollNoController.clear();
+        _emailController.clear();
+        _cgpaController.clear();
+        _phoneController.clear();
+      } else {
+        ScaffoldMessenger.of(
+          context,
+        ).showSnackBar(const SnackBar(content: Text('Failed to add student')));
+      }
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Add Student')),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Card(
+          elevation: 6,
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Form(
+              key: _formKey,
+              child: Column(
+                children: [
+                  TextFormField(
+                    controller: _nameController,
+                    decoration: const InputDecoration(
+                      labelText: 'Name',
+                      border: OutlineInputBorder(),
+                    ),
+                    validator:
+                        (value) => value!.isEmpty ? 'Please enter name' : null,
+                  ),
+                  const SizedBox(height: 16),
+                  TextFormField(
+                    controller: _rollNoController,
+                    keyboardType: TextInputType.number,
+                    decoration: const InputDecoration(
+                      labelText: 'Roll Number',
+                      border: OutlineInputBorder(),
+                    ),
+                    validator:
+                        (value) => value!.isEmpty ? 'Enter roll number' : null,
+                  ),
+                  const SizedBox(height: 16),
+                  TextFormField(
+                    controller: _emailController,
+                    decoration: const InputDecoration(
+                      labelText: 'email',
+                      border: OutlineInputBorder(),
+                    ),
+                    validator: (value) => value!.isEmpty ? 'Enter email' : null,
+                  ),
+                  const SizedBox(height: 16),
+                  TextFormField(
+                    controller: _emailController,
+                    decoration: const InputDecoration(
+                      labelText: 'phone',
+                      border: OutlineInputBorder(),
+                    ),
+                    validator: (value) => value!.isEmpty ? 'Enter phone' : null,
+                  ),
+                  const SizedBox(height: 24),
+                  ElevatedButton(
+                    onPressed: _saveStudent,
+                    style: ElevatedButton.styleFrom(
+                      minimumSize: const Size.fromHeight(50),
+                    ),
+                    child: const Text('Submit'),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
